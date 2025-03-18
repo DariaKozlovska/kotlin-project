3435e98997067d6c3d2aef58f9ce177cff6fae52
DariaKozlovska <odriy.lovska@gmail.com>
2025-03-18 12:12:39 +0100

    Add logo

--- app/src/main/java/com/example/dsw51762_kotlin/view/RegisterPage.kt
+++ app/src/main/java/com/example/dsw51762_kotlin/view/RegisterPage.kt
@@ -1,15 +1,38 @@
-package com.example.dsw51762_kotlin.view
+package com.example.testapp.views
 
+import android.graphics.Point
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+
 @Composable
 fun RegisterPage(){
+
+    var email by remember { mutableStateOf("") }
+
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
